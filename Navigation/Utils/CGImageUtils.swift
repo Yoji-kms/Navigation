8c2ca4409e8425a6383f2617da4f994ac4cdbd10
Yoji-kms <y0g@mail.ru>
2023-03-16 16:39:22 +0300

    Typo

--- Navigation/Utils/CGImageUtils.swift
+++ Navigation/Utils/CGImageUtils.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-extension Array<CGImage?> {
+extension [CGImage?] {
     func convertToUIImage() -> [UIImage] {
         return self.compactMap { image -> UIImage in
             guard let image = image else { fatalError("Error filtering image") }
